0969f8fb7cbd082312ee5259b2adfe0078fa87dd
PatrickCumpa <PatrickCumpa@INSPIRON.fritz.box>
2022-10-10 18:50:38 +0200

    Creazione server es. 21

---
+++ ServerEs21/src/serveres21/ServerEs21.java
@@ -0,0 +1,16 @@
+package serveres21;
+
+/**
+ *
+ * @author PatrickCumpa
+ */
+public class ServerEs21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
