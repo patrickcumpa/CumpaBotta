1ec7e6127bcda518cde0a6bf87848b9a64b8c789
PatrickCumpa <PatrickCumpa@INSPIRON.fritz.box>
2022-10-14 11:51:11 +0200

    Server finito

--- Es21/src/es21/TCPServer/TCPServer.java
+++ Es21/src/es21/TCPServer/TCPServer.java
@@ -2,17 +2,14 @@ package es21.TCPServer;
 
 import es21.Giudice;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,55 +19,58 @@ import java.util.logging.Logger;
  */
 public class TCPServer implements Runnable {
 
+    private DataInputStream dataIn;
+    private DataOutputStream dataOut;
     private final ServerSocket server;
-    private ObjectInputStream objIn;
-    private ObjectOutputStream objOut;
-    private ArrayList<Giudice> giudici;
 
     public TCPServer(int port) throws IOException {
         this.server = new ServerSocket(port);
-        this.giudici = new ArrayList<>();
     }
 
     @Override
     public void run() {
         Socket connection = null;
         System.out.println("Server in attesa...\n");
+        try {
+            connection = server.accept();
+            System.out.println(connection + " si è connesso!\n");
+            dataIn = new DataInputStream(connection.getInputStream());
+            dataOut = new DataOutputStream(connection.getOutputStream());
 
-        while (!Thread.interrupted()) {
-            try {
-                connection = server.accept();
-                objIn = new ObjectInputStream(connection.getInputStream());
-                objOut = new ObjectOutputStream(connection.getOutputStream());
-                System.out.println(connection + " si è connesso!\n");
+            while (!Thread.interrupted()) {
 
-                int scelta = (int) objIn.readObject();
-
-                switch (scelta) {
+                switch (dataIn.readInt()) {
                     case 1:
-                        giudici.add((Giudice) objIn.readObject());
-                        salvaDati();
-                        objOut.writeObject("\nVoti registrati con successo!\n");
-                        leggiDati();
+                        if (!contaGiudici()) {
+                            dataOut.writeUTF("\nSiamo spiacenti, ma l'archivio e' pieno!\n");
+                            break;
+                        }
+                        dataOut.writeUTF("Inserisci il tuo nome: ");
+                        Giudice giudice = new Giudice(dataIn.readUTF());
+                        for (int i = 0; i < 4; i++) {
+                            giudice.setVoti(dataIn.readInt());
+                        }
+                        salvaDati(giudice);
+
+                        dataOut.writeUTF("\nVoti registrati con successo!\n");
                         break;
-                        
+
                     case 2:
+                        dataOut.writeDouble(votoMedio((dataIn.readInt())));
                         break;
-                        
+
                     case 3:
                         Thread.currentThread().interrupt();
                         break;
-                        
+
                     default:
-                        objOut.writeObject("\nPer favore inserire una scelta valida!\n");
-                        objOut.flush();
+                        dataOut.writeUTF("\nPer favore inserire una scelta valida!\n");
                 }
-            } catch (IOException ex) {
-                System.err.println("Errore nella comunicazione con il client: " + ex);
-            } catch (ClassNotFoundException ex) {
-                Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
             }
+        } catch (IOException e) {
+            System.err.println("Errore nella comunicazione con il client: " + e);
         }
+
         try {
             server.close();
         } catch (IOException ex) {
@@ -78,23 +78,60 @@ public class TCPServer implements Runnable {
         }
     }
 
-    private void salvaDati() throws IOException {
-        FileWriter file = new FileWriter("dati.csv", true);
-        for (Iterator<Giudice> iterator = giudici.iterator(); iterator.hasNext();) {
-            file.write(iterator.next().toString());
-            file.write("\r\n");
+    private boolean contaGiudici() {
+        String line;
+        int count = -1;
+        BufferedReader reader;
+
+        try {
+            reader = new BufferedReader(new FileReader("dati.csv"));
+            while ((line = reader.readLine()) != null) 
+                count++;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        file.close();
+        return count < 10;
     }
 
-    private void leggiDati() throws FileNotFoundException, IOException {
-        BufferedReader sc = new BufferedReader(new FileReader("dati.csv"));
-        String line = "";
-        while ((line = sc.readLine()) != null) {
-            String[] tokens = line.split(",");
-            System.out.println(Arrays.toString(tokens));
+    private void salvaDati(Giudice giudice) {
+        FileWriter file = null;
+
+        try {
+            file = new FileWriter("dati.csv", true);
+            file.write(giudice.toString());
+            file.write("\r\n");
+        } catch (IOException ex) {
+            Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                file.close();
+            } catch (IOException ex) {
+                Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-        sc.close();
+    }
+
+    private double votoMedio(int codice) {
+        int count = 0;
+        int numero = 0;
+        BufferedReader reader;
+
+        try {
+            reader = new BufferedReader(new FileReader("dati.csv"));
+            String line = reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                String[] b = line.split(",");
+                numero += Integer.parseInt(b[codice]);
+                count++;
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return numero / (double) count;
     }
 
 }
