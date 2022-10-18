2d2d723dcc04cf242272423e71040775398df9ec
PatrickCumpa <PatrickCumpa@INSPIRON.fritz.box>
2022-10-18 23:08:54 +0200

    modifica di alcune costanti

---
+++ ServerEs20/src/serveres20/costanti/Costanti.java
@@ -0,0 +1,18 @@
+package serveres20.costanti;
+
+/**
+ *
+ * @author PatrickCumpa
+ */
+public class Costanti {
+    
+    public static final int SERVER_PORT = 12345;
+    public static final String REGISTRARE = "\nSiamo spiacenti, ma non risulti essere registrato.\nVuoi registrarti? [Y/n] ";
+    public static final String REGISTRATO = "Registrazione tessera avvenuta con successo.\n";
+    public static final String NON_REGISTRATO = "\nSiamo spiacenti, ma non risulti essere registrato.\n";
+    public static final String ESISTE = "\nSiamo spiacenti, ma queste utente esiste gia'.\n";
+    public static final String APERTO = "\nCassonetto aperto!\nArrivederci\n";
+    public static final String ELIMINARE = "\nVuoi eliminare la tua tessera? [Y/n] ";
+    public static final String ELIMINATO = "Eliminazione tessera avvenuto con successo.\n";
+    
+}
